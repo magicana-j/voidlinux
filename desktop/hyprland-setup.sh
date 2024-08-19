f71cf1c62d7be5a1864f878bde74ab85ac6d4eb2
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-08-20 06:57:43 +0900

    Update hyprland-setup.sh

--- desktop/hyprland-setup.sh
+++ desktop/hyprland-setup.sh
@@ -1,4 +1,5 @@
 sudo xbps-install -Su
 sudo xbps-install lightdm lightdm-gtk-greeter
-sudo xbps-install alacritty feh font-awesome xorg-xrandr grim slurp dolphin thunar cliphist 
-sudo xbps-install hyprland hyprpaper hypridle hyprlock swaylock swww mako xorg-desktop-portal-hyprland xorg-xwayland wofi
+sudo xbps-install alacritty feh font-awesome arandr xrandr grim slurp dolphin thunar cliphist
+sudo xbps-install swaylock swww mako xorg-desktop-portal-hyprland xorg-xwayland wofi
+sudo xbps-install hyprland hyprpaper hypridle hyprlock hyprshot
