3fc46be86f9c4408c605c298591c5db5f810fbcc
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-08-19 20:44:00 +0900

    Update i3wm-setup.sh

--- desktop/i3wm-setup.sh
+++ desktop/i3wm-setup.sh
@@ -1,4 +1,4 @@
 sudo xbps-install -Su
 sudo xbps-install lightdm lightdm-gtk-greeter
-sudo xbps-install dmenu i3 i3blocks i3lock i3status alakritty wezterm font-awesome xrandr rofi imagemagick
+sudo xbps-install dmenu i3 i3blocks i3lock i3status alacritty wezterm font-awesome xrandr rofi imagemagick
 sudo xbps-install dunst
