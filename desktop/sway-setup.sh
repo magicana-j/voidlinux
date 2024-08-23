b4625f97ceb9bafea6033ed9790027639907007c
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-08-23 09:12:32 +0900

    Update sway-setup.sh

--- desktop/sway-setup.sh
+++ desktop/sway-setup.sh
@@ -3,6 +3,6 @@ sudo xbps-install lightdm lightdm-gtk-greeter
 sudo xbps-install brightnessctl pavucontrol
 sudo xbps-install dmenu wofi
 sudo xbps-install foot alacritty
-sudo xbps-install font-awesome grim slurp swaybg thunar xrandr arandr imagemagick autotiling
+sudo xbps-install font-awesome grim slurp swaybg thunar xrandr arandr ImageMagick autotiling
 sudo xbps-install sway swayidle swaylock waybar xorg-xwayland
 sudo xbps-install mako
