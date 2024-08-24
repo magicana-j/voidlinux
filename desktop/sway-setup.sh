179ca69f023d389adf41c43af6e71f3345cb8f6f
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-08-24 15:28:25 +0900

    Add files via upload

--- desktop/sway-setup.sh
+++ desktop/sway-setup.sh
@@ -1,8 +1,10 @@
 sudo xbps-install -Su
-sudo xbps-install lightdm lightdm-gtk-greeter
-sudo xbps-install brightnessctl pavucontrol
-sudo xbps-install dmenu wofi
-sudo xbps-install foot alacritty
-sudo xbps-install font-awesome grim slurp swaybg thunar xrandr arandr ImageMagick autotiling
-sudo xbps-install sway swayidle swaylock waybar xorg-xwayland
-sudo xbps-install mako
+#sudo xbps-install -S lightdm lightdm-gtk-greeter
+#sudo xbps-install -S brightnessctl pavucontrol
+sudo xbps-install -S dmenu wofi
+sudo xbps-install -S foot alacritty
+sudo xbps-install -S font-awesome grim slurp swaybg
+#sudo xbps-install -S Thunar
+sudo xbps-install -S xrandr arandr ImageMagick autotiling
+sudo xbps-install -S sway swayidle swaylock Waybar xorg-xwayland
+#sudo xbps-install mako
