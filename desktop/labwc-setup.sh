ee45640887050f5598006894f3c8e06371a78719
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-29 16:23:20 +0900

    Update labwc-setup.sh

--- desktop/labwc-setup.sh
+++ desktop/labwc-setup.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 sudo xbps-install -Su
-sudo xbps-install labwc alacritty foot swaylock swaybg swayidle ngw-launcher Thunar thunar-archive-plugin Waybar grim slurp wlopm mako font-awesome6
+sudo xbps-install labwc alacritty foot swaylock swaybg swayidle nwg-launchers Thunar thunar-archive-plugin Waybar grim slurp wlopm mako font-awesome6
 
 mkdir -p ~/.config/labwc
 mkdir -p ~/.config/waybar
+mkdir -p ~/.config/foot
 cp /usr/share/doc/labwc/* ~/.config/labwc/
 cp /etc/xdg/waybar/* ~/.config/waybar/
+cp /etc/xdg/foot/foot.ini ~/.config/foot/
 
 cat << EOF >> ~/.config/labwc/environment
 XKB_DEFAULT_LAYOUT=jp
