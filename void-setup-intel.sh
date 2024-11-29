a37a23a859d8d72094b5af5b147a6c2ab0851703
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-29 16:19:58 +0900

    Update void-setup-intel.sh

--- void-setup-intel.sh
+++ void-setup-intel.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 sudo xbps-install -Su
-sudo xbps-install --force dbus elogind polkit
-sudo xbps-install --force NetworkManager network-manager-applet
-sudo xbps-install --force pipewire wireplumber pipewire-pulse pavucontrol bluez libspa-bluetooth
-sudo xbps-install --force mesa-dri libva-intel-driver mesa-intel-dri mesa-vulkan-intel ntfs-3g autofs x264 openh264
-sudo xbps-install --force vim neovim neofetch fastfetch htop curl zip unzip gvfs xarchiver p7zip
-sudo xbps-install --force noto-fonts-ttf noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf-extra 
-sudo xbps-install --force xprop fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-gtk fcitx5-gtk+ fcitx5-gtk+2 fcitx5-gtk+3
-sudo xbps-install --force xorg wayland xorg-server-xwayland
-sudo xbps-install --force shotwell firefox timeshift psensor shotcut vlc ssr
+sudo xbps-install dbus elogind polkit
+sudo xbps-install NetworkManager network-manager-applet
+sudo xbps-install pipewire wireplumber pipewire-pulse pavucontrol bluez libspa-bluetooth
+sudo xbps-install mesa-dri libva-intel-driver mesa-intel-dri mesa-vulkan-intel ntfs-3g autofs x264 openh264
+sudo xbps-install vim neovim neofetch fastfetch htop curl zip unzip gvfs xarchiver p7zip
+sudo xbps-install noto-fonts-ttf noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf-extra 
+sudo xbps-install xprop fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-gtk fcitx5-gtk+ fcitx5-gtk+2 fcitx5-gtk+3
+sudo xbps-install xorg wayland xorg-server-xwayland
+sudo xbps-install shotwell firefox timeshift psensor shotcut vlc ssr
 
 sudo ln -svnf /etc/sv/dbus /var/service/
 sudo ln -svnf /etc/sv/elogind /var/service/
