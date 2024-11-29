82ab8d92ba876c98c4c7a12bfa7afbc3c1d1dda0
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-29 18:00:39 +0900

    void-setup-intel.sh を更新

--- void-setup-intel.sh
+++ void-setup-intel.sh
@@ -8,7 +8,7 @@ sudo xbps-install mesa-dri libva-intel-driver mesa-intel-dri mesa-vulkan-intel n
 sudo xbps-install vim neovim neofetch fastfetch htop curl zip unzip gvfs xarchiver p7zip
 sudo xbps-install noto-fonts-ttf noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf-extra 
 sudo xbps-install xorg wayland xorg-server-xwayland
-sudo xbps-install shotwell firefox timeshift psensor shotcut vlc ssr
+sudo xbps-install shotwell firefox timeshift psensor shotcut vlc
 
 sudo ln -svnf /etc/sv/dbus /var/service/
 sudo ln -svnf /etc/sv/elogind /var/service/
