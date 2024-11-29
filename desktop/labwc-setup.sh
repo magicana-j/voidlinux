37a2cb3c5973cdcc0e394ec4b0f4cde2590c9b53
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-29 14:39:34 +0900

    Update labwc-setup.sh

--- desktop/labwc-setup.sh
+++ desktop/labwc-setup.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 sudo xbps-install -Su
-sudo xbps-install labwc swaylock swaybg swayidle ngw-launcher waybar grim slurp wlopm mako
+sudo xbps-install labwc swaylock swaybg swayidle ngw-launcher Thunar thunar-archive-plugin waybar grim slurp wlopm mako font-awesome6
 
 mkdir -p ~/.config/labwc
 mkdir -p ~/.config/waybar
 cp /usr/share/doc/labwc/* ~/.config/labwc/
+cp /etc/xdg/waybar/* ~/.config/waybar/
 
 cat << EOF >> ~/.config/labwc/environment
+XKB_DEFAULT_LAYOUT=jp
 XMODIFIERS=@im=fcitx
 EOF
