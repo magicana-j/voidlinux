0a2f009bed481ca11d2585fc4e5b0b5956421ea8
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-29 17:06:59 +0900

    void-setup-intel.sh を更新

--- void-setup-intel.sh
+++ void-setup-intel.sh
@@ -3,7 +3,7 @@
 sudo xbps-install -Su
 sudo xbps-install dbus elogind polkit
 sudo xbps-install NetworkManager network-manager-applet
-sudo xbps-install pipewire wireplumber pipewire-pulse pavucontrol bluez libspa-bluetooth
+sudo xbps-install pulseaudio pavucontrol bluez libspa-bluetooth
 sudo xbps-install mesa-dri libva-intel-driver mesa-intel-dri mesa-vulkan-intel ntfs-3g autofs x264 openh264
 sudo xbps-install vim neovim neofetch fastfetch htop curl zip unzip gvfs xarchiver p7zip
 sudo xbps-install noto-fonts-ttf noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf-extra 
