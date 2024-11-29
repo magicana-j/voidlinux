6071fd9568f74ea5e3bc854d38c02f8007e9125c
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-11-29 14:44:07 +0900

    Update void-setup.sh

--- void-setup.sh
+++ void-setup.sh
@@ -4,7 +4,7 @@ sudo xbps-install -Su
 sudo xbps-install --force dbus elogind polkit
 sudo xbps-install --force NetworkManager network-manager-applet
 sudo xbps-install --force pipewire wireplumber pipewire-pulse pavucontrol bluez libspa-bluetooth
-sudo xbps-install --force mesa-dri libva-intel-driver mesa-intel-dri mesa-vulkan-intel ntfs-3g autofs
+sudo xbps-install --force mesa-dri libva-intel-driver mesa-intel-dri mesa-vulkan-intel ntfs-3g autofs x264 openh264
 sudo xbps-install --force vim neovim neofetch fastfetch htop curl zip unzip gvfs xarchiver p7zip
 sudo xbps-install --force noto-fonts-ttf noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf-extra 
 sudo xbps-install --force xprop fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-gtk fcitx5-gtk+ fcitx5-gtk+2 fcitx5-gtk+3
