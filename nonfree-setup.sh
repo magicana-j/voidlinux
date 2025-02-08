8b2ed0276ae4a927ebe8174dd27cbda665adb51e
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-09 08:28:17 +0900

    Update nonfree-setup.sh

--- nonfree-setup.sh
+++ nonfree-setup.sh
@@ -1,2 +1,2 @@
 sudo xbps-install -Su
-sudo xbps-install -S void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
+sudo xbps-install void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
