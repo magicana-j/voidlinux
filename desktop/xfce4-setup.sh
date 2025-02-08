85f929b93e4cbf773541b6ee6f66a3653036d2aa
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-08 17:58:43 +0900

    Update xfce4-setup.sh

--- desktop/xfce4-setup.sh
+++ desktop/xfce4-setup.sh
@@ -1,3 +1,3 @@
 sudo xbps-install -Su
 sudo xbps-install lightdm lightdm-gtk-greeter
-sudo xbps-install xfce4 xfce4-plugins gvfs xarchiver xfce4-volumed-pulse
+sudo xbps-install xfce4 xfce4-plugins gvfs xarchiver thunar-archive-plugin
