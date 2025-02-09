47256938ee8392989613d15a6f3cedb5f4bd2f53
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-09 15:42:50 +0900

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
-sudo xbps-install flatpak
+echo 'Installing flatpak ...'
+sudo xbps-install -y flatpak
+echo 'Adding flathub repository ...'
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Warehouse
+echo 'Installing warehouse ...'
 flatpak install -y flathub io.github.flattool.Warehouse
 
 # OBS Studio
+echo 'Installing OBS Studio ...'
 flatpak install -y flathub com.obsproject.Studio
 
 # GPU Screen Recorder
+echo 'Installing GPU Screen Recorder ...'
 flatpak install -y flathub com.dec05eba.gpu_screen_recorder
 
 # VokoscreenNG
+echo 'Installing VokoscreenNG ...'
 flatpak install -y flathub com.github.vkohaupt.vokoscreenNG
 
 # Easy Effects
+echo 'Installing Easy Effects ...'
 flatpak install -y flathub com.github.wwmm.easyeffects
