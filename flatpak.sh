0f8d09a1dba9f6b957bd2e16a3e68a74a3bd3b69
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-09 14:58:15 +0900

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -2,5 +2,17 @@
 sudo xbps-install flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-flatpak install flathub com.obsproject.Studio
+# Warehouse
 flatpak install -y flathub io.github.flattool.Warehouse
+
+# OBS Studio
+flatpak install -y flathub com.obsproject.Studio
+
+# GPU Screen Recorder
+flatpak install -y flathub com.dec05eba.gpu_screen_recorder
+
+# VokoscreenNG
+flatpak install -y flathub com.github.vkohaupt.vokoscreenNG
+
+# Easy Effects
+flatpak install -y flathub com.github.wwmm.easyeffects
