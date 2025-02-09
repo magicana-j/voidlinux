dc8f05196e5dad9cc0bc09dcd5bf00e88f7f2068
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-09 15:46:42 +0900

    Update services.sh

--- install-scripts/services.sh
+++ install-scripts/services.sh
@@ -3,6 +3,6 @@
 target="/var/service/"
 
 sudo ln -svn /etc/sv/dbus $target
-# sudo ln -svn /etc/sv/elogind $target
+sudo ln -svn /etc/sv/elogind $target
 sudo ln -svn /etc/sv/bluetoothd $target
 sudo ln -svn /etc/sv/NetworkManager $target
