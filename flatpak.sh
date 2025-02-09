668866e4a4c749a1ecd1b0597f91a676daf0e557
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-02-09 13:44:52 +0900

    flatpak.sh を更新

--- flatpak.sh
+++ flatpak.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 sudo xbps-install flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub com.obsproject.Studio
+flatpak install -y flathub io.github.flattool.Warehouse
