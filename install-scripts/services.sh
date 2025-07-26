04effdeb8efeccf00bffd316b025a96bb6434c4e
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-07-27 08:57:00 +0900

    Delete install-scripts directory

--- install-scripts/services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-target="/var/service/"
-
-sudo ln -svn /etc/sv/dbus $target
-sudo ln -svn /etc/sv/elogind $target
-sudo ln -svn /etc/sv/bluetoothd $target
-sudo ln -svn /etc/sv/NetworkManager $target
