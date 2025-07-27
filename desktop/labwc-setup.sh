42d555a52c3e9562c3f4ceb843f44d14bcae4dde
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-07-27 09:23:19 +0900

    Update labwc-setup.sh

--- desktop/labwc-setup.sh
+++ desktop/labwc-setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 sudo xbps-install -Su
-sudo xbps-install labwc alacritty foot swaylock swaybg swayidle nwg-launchers Thunar thunar-archive-plugin Waybar grim slurp wlopm mako font-awesome6
+sudo xbps-install labwc alacritty foot swaylock swaybg swayidle wofi fuzzel Thunar thunar-archive-plugin Waybar grim slurp mako dunst font-awesome font-fira-ttf
 
-mkdir -p ~/.config/labwc
-mkdir -p ~/.config/waybar
-mkdir -p ~/.config/foot
-cp /usr/share/doc/labwc/* ~/.config/labwc/
-cp /etc/xdg/waybar/* ~/.config/waybar/
-cp /etc/xdg/foot/foot.ini ~/.config/foot/
+#mkdir -p ~/.config/labwc
+#mkdir -p ~/.config/waybar
+#mkdir -p ~/.config/foot
+#cp /usr/share/doc/labwc/* ~/.config/labwc/
+#cp /etc/xdg/waybar/* ~/.config/waybar/
+#cp /etc/xdg/foot/foot.ini ~/.config/foot/
 
-cat << EOF >> ~/.config/labwc/environment
-XKB_DEFAULT_LAYOUT=jp
-XMODIFIERS=@im=fcitx
-EOF
+#cat << EOF >> ~/.config/labwc/environment
+#XKB_DEFAULT_LAYOUT=jp
+#XMODIFIERS=@im=fcitx
+#EOF
