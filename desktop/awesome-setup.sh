0dbd556a7217ff214105490c9d6c68259a134f1c
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-07-27 09:26:20 +0900

    Delete desktop/awesome-setup.sh

--- desktop/awesome-setup.sh
+++
@@ -1,3 +0,0 @@
-sudo xbps-install -Su
-sudo xbps-install lightdm lightdm-gtk-greeter
-sudo xbps-install alacritty awesome feh font-awesome slock terminus-font liberation-fonts-ttf xrandr xsel xterm dunst
