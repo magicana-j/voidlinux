3a451a517906b4987e684ba4b188249efd226202
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-07-27 17:18:50 +0900

    hyprland-setup.sh を更新

--- desktop/hyprland-setup.sh
+++ desktop/hyprland-setup.sh
@@ -2,4 +2,4 @@ sudo xbps-install -Su
 #sudo xbps-install -y sddm
 sudo xbps-install -y alacritty foot feh font-awesome font-fira-ttf arandr xrandr grim slurp dolphin
 sudo xbps-install -y swaylock dunst mako xorg-desktop-portal-hyprland fuzzel wofi
-sudo xbps-install -y hyprland hyprpaper hypridle hyprlock hyprshot
+sudo xbps-install -y hyprland hyprpaper hypridle hyprlock hyprshot wlogout
