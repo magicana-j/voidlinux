3f4271ff5b1283a96b029f5fc7948f1e3fca8726
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2025-07-27 12:01:43 +0900

    Update config-networkmanager.sh

--- config-networkmanager.sh
+++ config-networkmanager.sh
@@ -4,4 +4,7 @@ sudo cat << EOF >> /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
 [connection]
 wifi.powersave = 2
 EOF
+sudo cat << EOF >> /etc/NetworkManager/conf.d/disable-ipv6.conf
+[connection]
+ipv6.method = ignore
 sudo sv restart NetworkManager
