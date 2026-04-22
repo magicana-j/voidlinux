94758bf1f31acefed8202b189409c67a5b44330f
magicana-j <magicana-j@example.dom>
2026-04-22 22:15:40 +0900

    Modify pipewire script

--- scripts/pipewire.sh
+++ scripts/pipewire.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xbps-query pulseaudio >/dev/null 2>&1 && xbps-remove -Ry pulseaudio
-xbps-query pipewire >/dev/null 2>&1 || xbps-install -y pipewire wireplumber libpulseaudio alsa-pipewire alsa-plugins-pulseaudio
+xbps-query pipewire >/dev/null 2>&1 || xbps-install -y pipewire wireplumber pavucontrol libpulseaudio alsa-pipewire alsa-plugins-pulseaudio
 
 echo "Configuring Pipewire integration..."
 # Configure Pipewire session manager
@@ -10,7 +10,6 @@ mkdir -p /etc/pipewire/pipewire.conf.d
 # Modify Pipewire configuration to launch pipewire-pulse
 [ -L /etc/pipewire/pipewire.conf.d/20-pipewire-pulse.conf ] || ln -sv /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
 
-# Configure ALSA to route audio through Pipewire
-mkdir -p /etc/alsa/conf.d
-[ -L /etc/alsa/conf.d/50-pipewire.conf ] || ln -sv /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d/
-[ -L /etc/alsa/conf.d/99-pipewire-default.conf ] || ln -sv /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
+# Autostart pipewire
+mkdir -p ~/.config/autostart
+cp ../pipewire/pipewire.desktop ~/.config/autostart/
--- scripts/pulse.sh
+++ scripts/pulse.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+rm -f ~/.config/autostart/pipewire.desktop
+
 xbps-query pipewire >/dev/null 2>&1 && xbps-remove -Ry pipewire wireplumber
 xbps-query pulseaudio >/dev/null 2>&1 || xbps-install -y pulseaudio pavucontrol libpulseaudio alsa-plugins-pulseaudio
