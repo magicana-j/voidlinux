f617bc2dfbb9f625e10b06b136f5de17ed1f095f
magicana-j <magicana-j@example.dom>
2026-04-23 18:16:08 +0900

    Modify NetworkManager scripts

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-xbps-install -y NetworkManager
+xbps-install -y iwd NetworkManager
 
+[ -L /var/service/iwd ] || ln -sv /etc/sv/iwd /var/service/
 [ -L /var/service/NetworkManager ] || ln -sv /etc/sv/NetworkManager /var/service/
-
-rm -f /var/service/dhcpcd
+rm -f /var/service/wpa_supplicant
--- scripts/nm_remove.sh
+++ scripts/nm_remove.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -f /var/service/NetworkManager
 
-[ -L /var/service/dhcpcd ] || ln -sv /etc/sv/dhcpcd /var/service/dhcpcd
+[ -L /var/service/wpa_supplicant ] || ln -sv /etc/sv/wpa_supplicant /var/service/
 
 xbps-query NetworkManager >/dev/null 2>&1 && xbps-remove -Ry NetworkManager
 xbps-query network-manager-applet >/dev/null 2>&1 && xbps-remove -Ry network-manager-applet
